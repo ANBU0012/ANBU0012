dbd7069ccb1d54c8302848ca62739a34550e71f2
ANBUclc0012 <73777725+ANBUclc0012@users.noreply.github.com>
2021-09-03 14:32:30 +0700

    ninh add crusg page /crush

---
+++ crush/config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    introTitle: 'Chào đồng chí',
+    introDesc: `Tôi hỏi đồng chí cái này, đồng chí trả lời thật lòng nha :))`,
+    btnIntro: 'Click nè',
+    title: 'Đồng chí có muốn tôi làm ny đồng chí khum?',
+    desc: 'Đồng chí không trả lời hoặc thoát là đồng chí nhận làm ny tôi rồi nhé :)',
+    btnYes: 'yêu ơi là iuuu <3',
+    btnNo: 'đồng chí mơ à:))',
+    question: 'Nói cho tới lý do vì sao đồng chí yêu tôi đi <3',
+    btnReply: 'Gửi cho Ninh',
+    reply: 'Tại vì đồng chí đẹp trai nhất thế giới',
+    mess: 'tôi biết mà hihi ',
+    messDesc: 'Còn giờ thì chờ gì nữa mà không inbox cho tôi đi nào.',
+    btnAccept: 'oke nha <3',
+    messLink: 'https://www.facebook.com/messages/t/ANBU0012/', //link fb
+    bgLink: 'https://c4.wallpaperflare.com/wallpaper/880/299/772/tree-heart-hearts-love-wallpaper-preview.jpg', //link ảnh nền
+    iputBgLink: 'https://huypham9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
+    lookMeLink: './img/bgimg.jpg', //link ảnh lookMe
+    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
+}
